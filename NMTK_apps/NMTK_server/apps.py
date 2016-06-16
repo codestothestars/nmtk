56ead418176299d73532233839096682389015d1
Chander Ganesan <chander@otg-nc.com>
2016-06-16 15:15:37 -0400

    Fix the app name

--- NMTK_apps/NMTK_server/apps.py
+++ NMTK_apps/NMTK_server/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class NMTK_serverConfig(AppConfig):
     name = 'NMTK_server'
-    verbose_name = 'Nonmotorized Toolkit Server'
+    verbose_name = 'Nonmotorized Travel Analysis Toolkit (NMTK) Server'
 
     def ready(self):
         from NMTK_server import signals
